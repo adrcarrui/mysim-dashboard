43054cdff542427036b89c59870cc22ad245277f
Adrián Cardona <adrian.cardonaruiz@atexis.com>
2026-09-11 14:12:17 +0200

    test(frontend): add coverage reporting to CI

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,4 +1,4 @@
-import js from '@eslint/js'
+﻿import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'coverage']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -20,3 +20,4 @@ export default defineConfig([
     },
   },
 ])
+
